04efb41c8b1c9062c7d779731afa7807bdd605f2
KUGAKU <githubgemini.0521@gmail.com>
2020-03-25 22:54:46 +0900

    implement easy display image center system

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Center(child: Text('Hello World')),
-    ));
+void main() => runApp(
+      MaterialApp(
+          home: Scaffold(
+        backgroundColor: Colors.blueGrey[100],
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[800],
+          title: Text('I Am Rich'),
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://images.idgesg.net/images/article/2019/11/idg-post-9-image-1-100817461-large.jpg'),
+          ),
+        ),
+      )),
+    );
